586305ad5cde8a15b80f4ecff587f29f0e904b1b
caffine_Overflow <tnzajk@gmail.com>
2024-12-07 21:33:23 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-function App() {
-  return <div></div>;
-}
+import React, { useState } from "react";
+import { CartProvider } from "./context/CartContext";
+import Header from "./components/Header";
+import ShoesAddForm from "./components/ShoesAddForm";
+import ShoeList from "./components/ShoeList";
+import CartModal from "./components/CartModal";
+
+const App = () => {
+  const [shoes, setShoes] = useState([]);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const handleAddShoe = (shoe) => {
+    setShoes((prev) => [...prev, shoe]);
+  };
+
+  const toggleCartModal = () => {
+    setIsCartOpen((prev) => !prev);
+  };
+
+  return (
+    <CartProvider>
+      <Header onCartClick={toggleCartModal} />
+      <ShoesAddForm onAddShoe={handleAddShoe} />
+      <ShoeList shoes={shoes} />
+      {isCartOpen && <CartModal onClose={toggleCartModal} />}
+    </CartProvider>
+  );
+};
 
 export default App;
